0c913e915ca3587440257dab5aa1b3033c15519d
ChangYong <lcy9002@naver.com>
2026-03-28 18:29:18 +0900

    yml 변경

--- password-domain/build.gradle.kts
+++ password-domain/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.github.gavlyukovskiy:p6spy-spring-boot-starter:2.0.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
